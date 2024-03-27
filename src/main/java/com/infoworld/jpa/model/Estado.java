6242833064fa13886f64fcb45c0b636130948b2d
Marcos Fernando <marcos.fersh@gmail.com>
2024-03-27 02:11:00 -0300

    Criacao das classes Cidade e Estado

---
+++ src/main/java/com/infoworld/jpa/model/Estado.java
@@ -0,0 +1,21 @@
+package com.infoworld.jpa.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Estado implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+}
